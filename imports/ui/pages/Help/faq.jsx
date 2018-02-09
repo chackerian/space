9a0f5e42a7b049b36649fc26252931457733f7fc
Nathan <nchackerian@gmail.com>
2018-02-09 17:18:46 -0600

    faq

--- imports/ui/pages/Help/faq.jsx
+++ imports/ui/pages/Help/faq.jsx
@@ -12,21 +12,7 @@ export default class AboutHelp extends Component {
 
         <h2>1.Initial Stage</h2>
 
-        The seller creates a new listing on the <a className="link" href='/add'>Add Listing</a> page and fills out all required information. This listing becomes viewable and searchable to all users.
-
-        <h2>2.Message Stage</h2>
-
-        The seller awaits contact from interested buyers. When a buyer views an item they potentially want, they may contact the seller through <a className="link" href='/chat'>Chat</a>.
-
-        On the <a className="link" href='/profile/{profile}/offers'>Sent Offers</a> and <a className="link" href='/profile/{profile}/listings'>Your Listings</a> pages, both the seller and buyer can monitor their own offers and listings.
-
-        <h2>3.Offer Stage</h2>
-
-        The buyer and seller decide on price, date, and location for their future meetup. Once they have decided on these details, the Buyer sends the Seller an Offer accessed from the Offer button on the listing's item page. The offer request has the following options: price, location, date, time, and any trade items if applicable. Once the Seller accepts the offer request, the meetup has been initiated.
-
-        <h2>4.Post Meetup Feedback Stage</h2>
-
-        After the meetup time has passed, both parties will receive a notification with a pending review. From here the feedback system kicks in. Each member of the meetup has the opportunity to evaluate each other. This feedback is saved to a user's profile for future reference.
+        Create a listing
 
         <h1>Do I need to provide a credit card, paypal account...etc?</h1>
 
